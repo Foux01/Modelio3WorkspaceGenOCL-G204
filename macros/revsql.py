c569556c9fca6311454613030ae508fa156d8e42
Schokobons <laetitia.r43@gmail.com>
2015-03-24 11:11:36 +0100

    fin seance1 projet

--- macros/revsql.py
+++ macros/revsql.py
@@ -1,4 +1,7 @@
 #GROUP204
+#SQL->UML
+
+#recuperation du fichier xml place tout dans root et on parcours root
 from xml.etree.ElementTree import ElementTree
 tree = ElementTree()
 
@@ -6,7 +9,48 @@ tree.parse("C:\Users\laetitia\Desktop\IDM\Modelio3WorkspaceGenOCL-G204\macros\li
 
 root = tree.getroot()
 
+
+#creation du model UML
+
+fact= theUMLFactory()
+myp = instanceNamed(Package,"MyPackage")
+fact.delete()
+
+
+def createClass(nameClass):
+
+    try:
+        trans = theSession().createTransaction("Class " + nameClass)
+        c1 = fact.createClass(nameClass,myp)
+        trans.commit()
+        trans.close()
+    except:
+        trans.rollback()
+        raise
+    
+    trans.close()
+        
+  
+def createAttribute(nameClass,nameAttribut):
+    try:
+        trans = theSession().createTransaction("Attribut " + nameAttribut)
+        class_ = instanceNamed(Class,nameClass)
+        a = fact.createAttribute(nameAttribut,class_)
+        trans.commit()
+    except:
+        trans.rollback()
+        raise
+    trans.close()
+        
+        
 for element in root:
     print (element.tag)
     for table in element:
-        print (table.attrib.get("name"))
+        Name = table.attrib.get("name")
+        print Name
+        # createClass(Name)
+        for attribute in table:
+            # createAttribute(Name,attribute.
+            print attribute.attrib.get("name")
+        
+        
